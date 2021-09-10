28b6162ddb7201599f8d8aa22ed370064f31cb9e
Quan Tran <quantm.cn@gmail.com>
2021-09-10 18:49:20 +1000

    Add use of local storage

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
 const themeSwitch = document.querySelector('.theme-switch');
 const createNewItemInput = document.querySelector('.input-container>input');
-const itemTemplate = document.querySelector('template');
 const itemList = document.querySelector('.item-list');
-const itemCountElement = document.querySelector('.count-and-clear');
 const clearCompletedBtn = document.querySelector('.clear-completed-btn');
 const filterBtns = document.querySelectorAll('.filter-btn');
 const [filterAllBtn, filterActiveBtn, filterCompletedBtn] = [...filterBtns];
 
 themeSwitch.addEventListener('click', () => {
 	document.body.classList.toggle('dark-theme');
+});
+
+const LOCAL_STORAGE_KEY = 'myTodoList';
+const items = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
+
+renderItemList();
+
+createNewItemInput.addEventListener('keyup', (e) => {
+	if (e.key === 'Enter') {
+		const input = createNewItemInput.value.trim();
+
+		if (input === '') return;
+
+		items.unshift({ detail: input, isChecked: false });
+		renderItemList();
+		updateItemsArrAndSaveToLocalStorage();
+
+		createNewItemInput.value = null;
+	}
+});
+
+clearCompletedBtn.addEventListener('click', () => {
+	const itemChecked = itemList.querySelectorAll('.checked');
+	itemChecked.forEach((item) => {
+		itemList.removeChild(item);
+	});
+
+	updateItemsArrAndSaveToLocalStorage();
+});
+
+filterBtns.forEach((btn) => {
+	btn.addEventListener('click', () => {
+		tickFilterBtn(btn);
+		displayFilteredItems();
+	});
 });
 
 function getAfterElement(y) {
@@ -39,11 +72,88 @@ function updateDraggables() {
 		const afterElement = getAfterElement(e.clientY);
 
 		if (afterElement === null) {
-			itemList.insertBefore(draggingElement, itemCountElement);
+			itemList.appendChild(draggingElement);
 		} else {
 			itemList.insertBefore(draggingElement, afterElement);
 		}
+
+		updateItemsArrAndSaveToLocalStorage();
 	});
+}
+
+function cloneTemplate(item) {
+	const clone = document.querySelector('template').content.cloneNode(true);
+
+	const itemContainer = clone.querySelector('.item-container');
+	const detailElement = clone.querySelector('.item-detail');
+	const checkBtn = clone.querySelector('.check-btn');
+	const clearBtn = clone.querySelector('.clear-btn');
+
+	detailElement.innerText = item.detail;
+
+	if (item.isChecked) {
+		itemContainer.classList.add('checked');
+	}
+
+	[detailElement, checkBtn].forEach((element) => {
+		element.addEventListener('click', () => {
+			itemContainer.classList.toggle('checked');
+			displayFilteredItems();
+			displayItemCount();
+			updateItemsArrAndSaveToLocalStorage();
+		});
+	});
+
+	clearBtn.addEventListener('click', () => {
+		itemList.removeChild(itemContainer);
+		displayItemCount();
+		updateItemsArrAndSaveToLocalStorage();
+	});
+
+	itemContainer.addEventListener('dragstart', () => {
+		itemContainer.classList.add('dragging');
+	});
+
+	itemContainer.addEventListener('dragend', () => {
+		itemContainer.classList.remove('dragging');
+	});
+
+	return clone;
+}
+
+function renderItemList() {
+	itemList.innerHTML = null;
+
+	items.forEach((item) => {
+		itemList.append(cloneTemplate(item));
+	});
+
+	updateDraggables();
+	displayFilteredItems();
+	displayItemCount();
+}
+
+function updateItemsArr() {
+	while (items.length > 0) {
+		items.pop();
+	}
+
+	const itemContainers = document.querySelectorAll('.item-container');
+	itemContainers.forEach((itemContainer) => {
+		const detail = itemContainer.querySelector('.item-detail').innerText;
+		const isChecked = itemContainer.classList.contains('checked');
+
+		items.push({ detail: detail, isChecked: isChecked });
+	});
+}
+
+function saveToLocalStorage() {
+	localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(items));
+}
+
+function updateItemsArrAndSaveToLocalStorage() {
+	updateItemsArr();
+	saveToLocalStorage();
 }
 
 function tickFilterBtn(clickedBtn) {
@@ -54,7 +164,7 @@ function tickFilterBtn(clickedBtn) {
 	clickedBtn.classList.add('ticked');
 }
 
-function updateItemStatus() {
+function displayFilteredItems() {
 	const items = itemList.querySelectorAll('.item-container');
 
 	if (filterActiveBtn.classList.contains('ticked')) {
@@ -84,72 +194,10 @@ function updateItemStatus() {
 	});
 }
 
-function updateItemCount() {
+function displayItemCount() {
 	const itemUnchecked = itemList.querySelectorAll('.item-container:not(.checked)');
-	var [numberElement, itemStringElement] = [...itemList.querySelectorAll('.item-count>span')];
+	var [numberElement, itemStringElement] = [...document.querySelectorAll('.item-count>span')];
 
 	numberElement.innerText = itemUnchecked.length;
 	itemStringElement.innerText = itemUnchecked.length > 1 ? 'items' : 'item';
 }
-
-createNewItemInput.addEventListener('keyup', (e) => {
-	if (e.key === 'Enter') {
-		const input = createNewItemInput.value.trim();
-
-		if (input === '') return;
-
-		const clone = itemTemplate.content.cloneNode(true);
-
-		const itemContainer = clone.querySelector('.item-container');
-		const detailElement = clone.querySelector('.item-detail');
-		const checkBtn = clone.querySelector('.check-btn');
-		const clearBtn = clone.querySelector('.clear-btn');
-
-		detailElement.innerText = input;
-
-		detailElement.addEventListener('click', () => {
-			itemContainer.classList.toggle('checked');
-			updateItemStatus();
-			updateItemCount();
-		});
-
-		checkBtn.addEventListener('click', () => {
-			itemContainer.classList.toggle('checked');
-			updateItemStatus();
-			updateItemCount();
-		});
-
-		clearBtn.addEventListener('click', () => {
-			itemList.removeChild(itemContainer);
-			updateItemCount();
-		});
-
-		itemContainer.addEventListener('dragstart', () => {
-			itemContainer.classList.add('dragging');
-		});
-
-		itemContainer.addEventListener('dragend', () => {
-			itemContainer.classList.remove('dragging');
-		});
-
-		itemList.prepend(clone);
-		createNewItemInput.value = null;
-		updateDraggables();
-		updateItemStatus();
-		updateItemCount();
-	}
-});
-
-clearCompletedBtn.addEventListener('click', () => {
-	const itemChecked = itemList.querySelectorAll('.checked');
-	itemChecked.forEach((item) => {
-		itemList.removeChild(item);
-	});
-});
-
-filterBtns.forEach((btn) => {
-	btn.addEventListener('click', () => {
-		tickFilterBtn(btn);
-		updateItemStatus();
-	});
-});
